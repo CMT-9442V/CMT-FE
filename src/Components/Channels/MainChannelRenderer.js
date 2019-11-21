608862246e5f3aaa61d9d9023bc90d1ca88e5ef2
Bryce-Soghigian <49734722+Bryce-Soghigian@users.noreply.github.com>
2019-11-21 02:26:39 -0600

    Update MainChannelRenderer.js

--- src/Components/Channels/MainChannelRenderer.js
+++ src/Components/Channels/MainChannelRenderer.js
@@ -68,7 +68,7 @@ export default function MainChannelRenderer() {
                 console.error(err)
             })
         }
-    }, [move,state])
+    }, [move])
    
     const KeyHandler = (e) => {
         console.log("renderer", e.key)
